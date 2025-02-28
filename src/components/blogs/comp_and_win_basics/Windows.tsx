8811da7e02145e9e54b4ae0085bb4a180e75ec3b
Thakur Anubhav Singh <anubhavthakur1996@outlook.com>
2025-02-28 15:32:42 +0530

    - Added content and assets to the win page

--- src/components/blogs/comp_and_win_basics/Windows.tsx
+++ src/components/blogs/comp_and_win_basics/Windows.tsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router";
 import { Typography, Button } from "antd";
 import Conf from "../../../assets/win/confused.png";
 import TaskBar from "../../../assets/win/task_bar.png";
+import Start from "../../../assets/win/start_button.png";
+import StartMenu from "../../../assets/win/start_menu.png";
 import Footer from "../../common/Footer";
 import TopicTitleSection from "../../common/TopicTitleSection";
 import QuestionSection from "../../common/QuestionSection";
@@ -43,12 +45,13 @@ const Windows: React.FC = (): JSX.Element => {
       </TopicTitleSection>
 
       <Typography.Paragraph>
-        {/* Operating System */}
+        {/* Windows */}
         <QuestionSection>What is Windows?</QuestionSection>
 
         {/* Confused */}
         <ImageSection ImageSrc={Conf} alt="Confused" caption={null} />
 
+        {/* Windows explanation */}
         <AnswerSection>
           Windows is an operating system made by Microsoft in 1985. It's been
           one of the most popular operating systems out there. There are many
@@ -70,20 +73,71 @@ const Windows: React.FC = (): JSX.Element => {
 
         {/* Task Bar */}
         <QuestionSection>Task Bar?</QuestionSection>
+
+        {/* Task bar explanation */}
         <AnswerSection>
-          The bottom most strip comtaining, Windows button, search and some
+          The bottom most strip containing, Windows button, search and some
           other pinned items is known as{" "}
           <TextHighlight>task bar.</TextHighlight> You can pin your own apps
           that you use often to the task bar, access search, start button and do
           much more.
         </AnswerSection>
+
         {/* Task Bar Image */}
         <ImageSection
           ImageSrc={TaskBar}
           alt="task_bar"
           caption="Task bar on Windows 11"
         />
+
+        {/* Start */}
+        <QuestionSection>Start (Start button)?</QuestionSection>
+
+        {/* Start explanation */}
+        <AnswerSection>
+          The four squares at the bottom on the{" "}
+          <TextHighlight>task bar</TextHighlight> together are called the{" "}
+          <TextHighlight>start</TextHighlight> or{" "}
+          <TextHighlight>start button.</TextHighlight> It's an iconic component
+          of Windows operating systems since the inception. Clicking this button
+          reveals some options including options to shut down, restart,
+          hibernate and sleep.
+        </AnswerSection>
+
+        {/* Start Button Image */}
+        <ImageSection
+          ImageSrc={Start}
+          alt="start button"
+          caption="Start button in Windows 11"
+        />
+
+        {/* Start Menu */}
+        <QuestionSection>Start Menu ?</QuestionSection>
+
+        {/* Start Menu Explanation */}
+        <AnswerSection>
+          The pop up menu that opens when we click on the{" "}
+          <TextHighlight>start button</TextHighlight> is called the start menu.
+          Since, it pops up when we click on the start button, it's called{" "}
+          <TextHighlight>start menu.</TextHighlight>
+        </AnswerSection>
+
+        {/* Start Menu Image */}
+        <ImageSection
+          ImageSrc={StartMenu}
+          alt="start menu"
+          caption="Start menu in Windows 11"
+        />
+
+        {/* File Explorer */}
+        <QuestionSection>What is a File Explorer?</QuestionSection>
+
+        {/* File explorer explanation */}
+        <AnswerSection>
+          
+        </AnswerSection>
       </Typography.Paragraph>
+
       <Footer {...props} />
     </div>
   );
